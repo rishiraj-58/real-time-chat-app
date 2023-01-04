5db9e03b104c178a6de198f4397f73198b46c4b1
rishiraj-58 <s.rishiraj2002@gmail.com>
2023-01-04 18:26:24 +0530

    Chat app

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,11 @@
+const { sendMessage, allMessages } = require("../controllers/messageControllers")
+const { protect } = require("../middleware/authMiddleware")
+
+const express = require("express")
+
+const router = express.Router()
+
+router.route('/').post(protect,sendMessage )
+router.route('/:chatId').get(protect,allMessages)
+
+module.exports = router
